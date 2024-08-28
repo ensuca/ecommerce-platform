c28b5b0f613058b12476d0ead12fab0a0d6bcdd0
ensuca <56631549+ensuca@users.noreply.github.com>
2024-08-28 08:04:33 +0300

    Create User.java
    for user management neccesery packages created

---
+++ src/main/java/com/enesuca/ecommerceplatform/user/model/User.java
@@ -0,0 +1,51 @@
+package com.enesuca.ecommerceplatform.user.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    // Getter ve Setter metodları (Lombok kullanıyorsan @Data ile otomatik olarak ekleyebilirsin)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
