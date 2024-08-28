49ca44d77aa7316a9a902da30035d4f52128c82d
ensuca <56631549+ensuca@users.noreply.github.com>
2024-08-28 07:56:07 +0300

    Project Started
    A microservices-based e-commerce platform

---
+++ src/main/java/com/enesuca/ecommerceplatform/EcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.enesuca.ecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enesuca/ecommerceplatform/EcommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesuca.ecommerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
