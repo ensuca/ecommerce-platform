0fc89ed964b9af1597c3e6cedfde6292ea8050a1
ensuca <56631549+ensuca@users.noreply.github.com>
2024-09-04 06:47:47 +0300

    Update EcommercePlatformApplication.java

--- src/main/java/com/enesuca/ecommerceplatform/EcommercePlatformApplication.java
+++ src/main/java/com/enesuca/ecommerceplatform/EcommercePlatformApplication.java
@@ -3,7 +3,7 @@ package com.enesuca.ecommerceplatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.enesuca.ecommerceplatform"})
 public class EcommercePlatformApplication {
 
     public static void main(String[] args) {
